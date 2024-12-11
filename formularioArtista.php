7e8471a4feee82a0d00b173ab8ceff41df68a449
Arturia Queiroz <93953466+Arturia1@users.noreply.github.com>
2024-12-11 15:28:00 -0300

    comitt

--- formularioArtista.php
+++ formularioArtista.php
@@ -29,7 +29,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulário | GN</title>
+    <title>Formulário Artista</title>
     <style>
         * {
             box-sizing: border-box;
--- formularioUsuario.php
+++ formularioUsuario.php
@@ -30,7 +30,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulário | GN</title>
+    <title>Formulário Usuário</title>
     <style>
         * {
             box-sizing: border-box;
